27890ceadcd3ae88f90809c3fd6f98ca91bce1a7
emilio <dlt.emilio@gmail.com>
2019-12-03 11:32:10 -0600

    amortization actualization

--- activities/classwork/amortization/main.py
+++ activities/classwork/amortization/main.py
@@ -2,23 +2,60 @@ import fire
 
 from util import Amortization
 
+import matplotlib.pyplot as plt
+
+
+
+
 
 class Main(object):
 
+
+
     @staticmethod
+
     def future_value(present_value, interest, n):
+
         return present_value * (1 + interest) ** n
 
+
+
     @staticmethod
+
     def annuity(amount, interest, n):
-        pass
+
+        amortization = Amortization(amount, interest, n)
+
+        return amortization.annuity
+
+
 
     def table(self, amount, interest, n, rows=10, save=""):
-        pass
+
+        amortization = Amortization(amount,interest,n)
+
+        df = amortization.get_table()
+
+        print(df)
+
+        if save:
+
+            df.to_csv(save)
+
+
 
     def graph(self, amount, interest, n, save=""):
-        pass
+
+        amortization = Amortization(amount, interest, n)
+
+        fig = amortization.get_plot()
+
+        if save:
+
+            fig.savefig(save)
+
 
 
 if __name__ == "__main__":
+
     fire.Fire(Main)
